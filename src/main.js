4eb998abe2dcc5b39e588bf3182aaeb7c43f36aa
Camilla Ridesjö <camilla.ridesjo@gmail.com>
2022-12-07 11:10:59 +0100

    Add questions js

--- src/main.js
+++ src/main.js
@@ -1,23 +1,86 @@
-import './style/style.scss';
+import 'style.scss';
 
 const gameDescText = 'Välkommen till vårt familje-quiz!';
 const descriptionText = document.querySelector('#descriptionText');
 
-descriptionText.innerHTML = descText;
+descriptionText.innerHTML = gameDescText;
 
 document.querySelector('#startGameBtn').addEventListener('click', startGame);
 
 let player = '';
 
 const questions = [
-    {
-        questionText: 'Vem hissades upp i flaggstången i Lönneberga?',
-        answerOptions: [
-            'Karl-Alfred',
-            'Emils mamma',
-            'Ida'
-        ],
-        correctAnswer: 'Ida',
-    },
+{
+    questionText: 'Vem hissades upp i flaggstången i Lönneberga?',
+    answerOptions: [
+        'Karl-Alfred',
+        'Emils mamma',
+        'Ida'
+    ],
+    correctAnswer: 'Ida',
+},
+{
+    questionText: 
+    answerOptions: [
+        '',
+        '',
+        ''
+    ],
+    correctAnswer: '',
+},
+{
+    questionText: 
+    answerOptions: [
+        '',
+        '',
+        ''
+    ],
+    correctAnswer: '',
+},
+{
+    questionText: 
+    answerOptions: [
+        '',
+        '',
+        ''
+    ],
+    correctAnswer: '',
+},
+{
+    questionText: 
+    answerOptions: [
+        '',
+        '',
+        ''
+    ],
+    correctAnswer: '',
+},
+{
+    questionText: 
+    answerOptions: [
+        '',
+        '',
+        ''
+    ],
+    correctAnswer: '',
+},
+{
+    questionText: 
+    answerOptions: [
+        '',
+        '',
+        ''
+    ],
+    correctAnswer: '',
+},
+{
+    questionText: 
+    answerOptions: [
+        '',
+        '',
+        ''
+    ],
+    correctAnswer: '',
+},
     
 ]
